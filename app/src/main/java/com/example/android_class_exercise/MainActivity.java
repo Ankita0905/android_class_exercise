0116863de30d62f933965ccf4eef857276489968
Ankita Jain <ankita.jain12342@gmail.com>
2019-11-26 21:06:05 -0500

    result activity

--- app/src/main/java/com/example/android_class_exercise/MainActivity.java
+++ app/src/main/java/com/example/android_class_exercise/MainActivity.java
@@ -2,15 +2,33 @@ package com.example.android_class_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText ios,swift,java,android,dbms;
+    Button Add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ios=findViewById(R.id.edtdbms);
+        swift=findViewById(R.id.edtswift);
+        java=findViewById(R.id.edtjava);
+        android=findViewById(R.id.edtandroid);
+        dbms=findViewById(R.id.edtdbms);
+        Add=findViewById(R.id.btnadd);
+
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent mIntent=new Intent(MainActivity.this,ResultActivity.class);
+            }
+        });
     }
 }
