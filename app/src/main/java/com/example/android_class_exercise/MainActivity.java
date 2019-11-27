660f7adc86ef71ad7b75cd5d896f350e7a0ca7a2
Ankita Jain <ankita.jain12342@gmail.com>
2019-11-26 20:51:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android_class_exercise/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android_class_exercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText ios,swift,java,android,dbms;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
